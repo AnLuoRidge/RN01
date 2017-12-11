abc1c5dd65a006eff2ddc3ee170cee99f2b21be0
Ding <anluoridge@gmail.com>
2017-12-11 16:53:40 +1100

    add some text

---
+++ albums/index.js
@@ -0,0 +1,25 @@
+// Index.ios.js - place code in here for iOS
+
+// Import a lib to help create a component
+import React from 'react';
+import ReactNative from 'react-native';
+
+
+// Create a component
+const App = () => {
+  return (
+      <Text>Some Text </Text>
+  )
+};
+
+
+// Render it to the device
+
+
+
+
+/*import { AppRegistry } from 'react-native';
+import App from './App';
+
+AppRegistry.registerComponent('albums', () => App);
+*/
