27e96557fa6bc3eaa12e30c8041a1f74a9460e1a
Ding <anluoridge@gmail.com>
2017-12-21 17:57:49 +1100

    install ESlint

--- albums/src/components/header.js
+++ albums/src/components/header.js
@@ -1,11 +1,18 @@
 // Import lib for making a component
-import React from 'react';
-import { Text } from 'react-native';
+import React from 'react'
+import { Text, View } from 'react-native'
 
 // Make a component
-const Header = () => (
-  <Text>Albums!</Text>
-);
+const Header = () => { // dont use => ()
+  const { textStyle } = styles // {} means object
+  return <Text style={textStyle}>Albums!</Text> // JSX
+}
+
+const styles = { // like CSS
+  textStyle: {
+    fontSize: 20
+  }
+}
 
 // Make the component available to other parts of the app
- export default Header;
+export default Header
