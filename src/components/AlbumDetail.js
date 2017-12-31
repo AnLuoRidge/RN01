d41da2a6e37c2066bed80a6679304592dc6f113f
Ding <anluoridge@gmail.com>
2017-12-31 18:01:51 +1100

    add artwork and enable scroll add styles

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -4,14 +4,20 @@ import Card from './Card'
 import CardSection from './CardSection'
 
 const AlbumDetail = ({album}) => {
-    const {title, thumbnail_image: thumbnail, artist} = album
-    const { headerContentStyle, thumbnailStyle } = styles
+    const {title, thumbnail_image: thumbnail, artist, image: artwork} = album
+    const {
+        headerContentStyle,
+        thumbContainerStyle,
+        thumbnailStyle,
+        artworkStyle
+    } = styles
 
+console.log(artwork)
     return (
 
         <Card>
             <CardSection>
-                <View>
+                <View style={thumbContainerStyle}>
                     <Image
                         style={thumbnailStyle}
                         source={{uri: thumbnail}}
@@ -22,18 +28,42 @@ const AlbumDetail = ({album}) => {
                     <Text>{artist}</Text>
                 </View>
             </CardSection>
+
+            <CardSection>
+                <View>
+                    <Image
+                        style={artworkStyle}
+                        source={{uri: artwork}}
+                        //resizeMode="contain"
+                    />
+                </View>
+            </CardSection>
         </Card>
     )
 }
 
 const styles = {
+    thumbnailStyle: {
+        height: 50,
+        width: 50
+    },
+    thumbContainerStyle: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginLeft: 10,
+        marginRight: 10
+    },
+    artworkStyle: {
+        height: 300,
+        // flex: 1, todo to fix
+        width: 300
+    },
     headerContentStyle: {
         flexDirection: 'column',
         justifyContent: 'space-around'
     },
-    thumbnailStyle: {
-        height: 50,
-        width: 50
+    headerTextStyle: {
+        fontSize: 18
     }
 }
 
--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { View } from 'react-native'
+import { ScrollView } from 'react-native'
 import AlbumDetail from './AlbumDetail';
 import axios from 'axios'
 
@@ -57,9 +57,9 @@ export default class AlbumList extends React.Component {
 
     render() {
         return (
-            <View>
+            <ScrollView>
                 {this.renderAlbums()}
-            </View>
+            </ScrollView>
         )
     }
 }
