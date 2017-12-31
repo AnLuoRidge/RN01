5fc11184c98331d867bb31cb1b076be2cae35f1f
Ding <anluoridge@gmail.com>
2017-12-31 11:50:02 +1100

    show titles on screen

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,13 +1,26 @@
 import React from 'react'
 import { Text, View } from 'react-native'
+import axios from 'axios'
 
-const AlbumList = () => (
-    <View>
-    <Text>pic holder</Text>
-    </View>
-)
+export default class AlbumList extends React.Component {
+    state = { albums:[] };
 
+    componentWillMount() {
+        axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+         .then(response => this.setState({albums: response.data}))
+         .catch(error => console.log(error))
+    }
 
+    renderAlbums() {
+        return this.state.albums.map(album => <Text>{album.title}</Text>)
+    }
 
-
-export default AlbumList;
+    render() {
+        console.log(this.state)
+        return (
+            <View>
+                {this.renderAlbums()}
+            </View>
+        )
+    }
+}
